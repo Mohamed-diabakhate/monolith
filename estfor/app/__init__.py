c39534594293f5121e55c61e10787a6b88137009
Mohamed-diabakhate <63637174+Mohamed-diabakhate@users.noreply.github.com>
2025-08-04 22:02:17 +0200

    Test solo developer CI/CD workflow

--- estfor/app/__init__.py
+++ estfor/app/__init__.py
@@ -7,3 +7,4 @@ A Docker-based system for collecting and storing EstFor Kingdom assets in Firest
 __version__ = "1.0.0"
 __author__ = "EstFor Team"
 __description__ = "Asset collection system for EstFor Kingdom" # trigger test
+# Solo Developer CI/CD Test
